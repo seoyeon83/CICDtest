65f9278f1511c77a7d9612d2f1b1152322a4e9f7
seoyeon83 <mool848777@naver.com>
2024-05-06 07:52:01 +0900

    init

---
+++ src/main/java/com/cicdtest/cit/CicdtestApplication.java
@@ -0,0 +1,13 @@
+package com.cicdtest.cit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdtestApplication.class, args);
+    }
+
+}
